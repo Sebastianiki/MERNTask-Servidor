d7c3c9713b4fcf6a3ca1a66b15ba3e289d76c8e3
Sebastianiki <reyesebastian1709@gmail.com>
2021-05-11 19:02:10 -0400

    ajustes port

--- index.js
+++ index.js
@@ -6,18 +6,17 @@ const app = express()
 
 connectDB()
 
-app.use(cors({ credentials: true, origin: true }));
-app.options("*", cors());   
+app.use(cors())
 
 app.use(express.json({ extend: true }))
 
-const PORT = process.env.port || 4001
+const port = process.env.port || 4001
 
 app.use('/api/usuarios', require('./routes/usuarios'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/proyectos', require('./routes/proyectos'))
 app.use('/api/tareas', require('./routes/tareas'))
 
-app.listen(PORT,'0.0.0.0', () => {
-    console.log(`El servidor esta funcionando en el puerto ${PORT}`)
+app.listen(port,'0.0.0.0', () => {
+    console.log(`El servidor esta funcionando en el puerto ${port}`)
 })
