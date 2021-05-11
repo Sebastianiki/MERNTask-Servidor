093f99ce6170636fb3d505679b9cc90cd466399c
Sebastianiki <reyesebastian1709@gmail.com>
2021-05-11 18:34:34 -0400

    ajustes

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ app.use(cors())
 
 app.use(express.json({ extend: true }))
 
-const PORT = process.env.PORT || 4001
+const port = process.env.port || 4001
 
 app.use('/api/usuarios', require('./routes/usuarios'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/proyectos', require('./routes/proyectos'))
 app.use('/api/tareas', require('./routes/tareas'))
 
-app.listen(PORT, () => {
+app.listen(port,'0.0.0.0', () => {
     console.log(`El servidor esta funcionando en el puerto ${PORT}`)
 })
